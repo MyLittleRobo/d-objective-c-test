5fe569da50cb605c94780d6c1db23cb5c123b19d
FreeSlave <freeslave93@gmail.com>
2016-04-30 16:29:32 +0300

    Add more  compilers

--- source/app.d
+++ source/app.d
@@ -1,17 +1,13 @@
 version(OSX) {
     static if (__VERSION__ > 2070)
     {
-        enum NSStringTempl = 
-        q{
-            extern (Objective-C)
-            interface NSString
-            {
-                NSString initWithUTF8String(in char* str) @selector("initWithUTF8String:");
-                const(char)* UTF8String() @selector("UTF8String");
-                void release() @selector("release");
-            }
-        };
-        mixin(NSStringTempl);
+        extern (Objective-C)
+        interface NSString
+        {
+            NSString initWithUTF8String(in char* str) @selector("initWithUTF8String:");
+            const(char)* UTF8String() @selector("UTF8String");
+            void release() @selector("release");
+        }
     }
 }
 
